3935112f2199f6c9cc6eabae16273074b74891d2
ivytreesa02 <93897818+ivytreesa02@users.noreply.github.com>
2022-10-28 22:32:11 +0530

    Add files via upload
    intial commit

---
+++ cloth_management/src/main/java/com/cloth_management/Models/Category.java
@@ -0,0 +1,29 @@
+package com.cloth_management.Models;
+
+public class Category {
+    public int cat_id;
+    public String cat_name;
+    public int getCat_id() {
+        return cat_id;
+    }
+
+    public void setCat_id(int cat_id) {
+        this.cat_id = cat_id;
+    }
+
+    public String getCat_name() {
+        return cat_name;
+    }
+
+    public void setCat_name(String cat_name) {
+        this.cat_name = cat_name;
+    }
+    @Override
+    public String toString() {
+        return "categories{" +
+                "cat_name='" + cat_name + '\'' +
+                '}';
+    }
+
+
+}
